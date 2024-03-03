88d57d108998e1846eeaad2d3022b28ee84322cd
Tanawat Pokkantha <flxke.tanawat@gmail.com>
2024-03-03 14:32:26 +0700

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard',
+    function (){
         return view('dashboard');
     })->name('dashboard');
-});
 
 Route::get('/redirect', [HomeController::class, 'redirect']);
